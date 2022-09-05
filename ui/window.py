353993e99dfb0638dc2a3516cd828683fdc9a148
Alex Warnes <warnes.alexander@gmail.com>
2022-09-05 15:36:15 -0700

    MCS-3: Add basic menu structure.

--- ui/window.py
+++ ui/window.py
@@ -1,15 +1,17 @@
-from PySide6.QtWidgets import QMainWindow, QLabel
-
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QKeySequence
+from PySide6.QtWidgets import QMainWindow, QLabel, QMenu
+from .menu import createMenuBar
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-        self.setupMenu()
-        
-        self.tempLabel = QLabel(parent=self)
-        self.tempLabel.setText("Hello World")
-        self.tempLabel.show()
+        self.setWindowTitle("MTG Card Scanner")
+        self.resize(800, 600)
 
-    def setupMenu(self):
-        pass
+        createMenuBar(self)
+
+        tempLabel = QLabel("Hello, World!")
+        tempLabel.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        self.setCentralWidget(tempLabel)
