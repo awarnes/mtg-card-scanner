1ee55258278fd0ec71d608075e5d75df7da39296
Alex Warnes <warnes.alexander@gmail.com>
2022-10-11 08:43:56 -0700

    MCS-8: Add automatic requirement discovery to setup.py.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import pathlib
 from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
@@ -28,23 +29,11 @@ setup(
     description="Desktop application for managing MTG card collections.",
     long_description=long_description,
     url="https://github.com/awarnes/mtg-card-scanner",
-    install_requires=[
-        "fuzzywuzzy==0.18.0"
-        "numpy==1.23.2"
-        "opencv-python==4.6.0.66"
-        "packaging==21.3"
-        "Pillow==9.2.0"
-        "pyparsing==3.0.9"
-        "PySide6==6.3.1"
-        "PySide6-Addons==6.3.1"
-        "PySide6-Essentials==6.3.1"
-        "pytesseract==0.3.10"
-        "shiboken6==6.3.1"
-    ],
+    install_requires=pathlib.Path("requirements.txt").open().readlines(),
     # https://stackoverflow.com/questions/20288711/post-install-script-with-python-setuptools
     cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand
+        "develop": PostDevelopCommand,
+        "install": PostInstallCommand
     }
 )
 
